0b1bef791f1bd2f61eed955e5e9b3b0b6d98e312
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2023-11-23 17:01:41 +0900

    Changes

---
+++ spring/src/main/java/umc/spring/domain/FoodCategory.java
@@ -0,0 +1,24 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
---
+++ spring/src/main/java/umc/spring/web/dto/TempResponse.java
@@ -0,0 +1,30 @@
+package umc.spring.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TempResponse {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempTestDTO{
+        String testString;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempExceptionDTO{
+        Integer flag;
+    }
+}
